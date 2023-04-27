deda52cf92a866c47d1cd4d0d3144ff9b3c7e768
smddlswn <smddlswn@gmail.com>
2023-04-27 09:31:12 +0900

    웹크롤링연습4

--- web1.py
+++ web1.py
@@ -8,5 +8,19 @@ page = open("c:\\work\\test01.html","rt",encoding="utf-8").read()
 
 #검색이 용이한 객체 생성
 soup = BeautifulSoup(page,"html.parser")
-print(soup.prettify())
+# print(soup.prettify())
+
+#<p>태그 전부 검색
+# print(soup.find_all("p"))
+#<p> 하나만 검색
+# print(soup.find("p"))
+#<p class="outer-text"> 조건이 있는 경우
+# print(soup.find_all("p",class_="outer-text"))
+# print(soup.find_all("p",attrs={"class":"outer-text"}))
+#태그 내부의 컨텐츠만 사용
+for item in soup.find_all("p"):
+    #태그는 버리고 컨텐츠만 사용
+    title = item.text.strip()
+    title = title.replace("\n","")
+    print(title)
 
